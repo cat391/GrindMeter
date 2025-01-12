b33c0d4ce02ff89737085be02e47928ae3832ad9
Cole <110432006+cat391@users.noreply.github.com>
2025-01-12 16:43:07 -0500

    Updated coloring of Timer

--- grindmeter/src/components/Timer.jsx
+++ grindmeter/src/components/Timer.jsx
@@ -13,7 +13,7 @@ export default function Timer({ duration, isRunning, reset }) {
       className={
         state
           ? "flex items-center justify-center h-96 w-full text-8xl font-bold text-red-700"
-          : "flex items-center justify-center h-96 w-full text-8xl font-bold text-gray-100"
+          : "flex items-center justify-center h-96 w-full text-8xl font-bold text-customBlack-400"
       }
     >
       {String(hours).padStart(2, "0")}:{String(minutes).padStart(2, "0")}:
