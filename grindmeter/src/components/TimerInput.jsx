cdbafa7926f47433c221797bdf577cacd67e8c5c
Cole <110432006+cat391@users.noreply.github.com>
2025-01-19 21:57:54 -0500

    Update TimerInput.jsx

--- grindmeter/src/components/TimerInput.jsx
+++ grindmeter/src/components/TimerInput.jsx
@@ -3,6 +3,7 @@ export default function TimerInput({ value = "00:00:00" }) {
   // The value should be in format 00:00:00
   // However, when you change you keep the format
   // Do this with the value handleClick logic i did earlier
+
   return (
     <div>
       <input type="text" placeholder={value} />
